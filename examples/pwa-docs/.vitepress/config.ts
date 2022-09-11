f0b333f5082a878b34c9e822bd47e652bab29a6d
userquin <userquin@gmail.com>
2022-09-11 16:48:54 +0200

    chore: configure pwa properly chore: add some more entries chore: use script setup in md pages to inject NOW via `vite-define` chore: add d.ts for vite env (vite.define)

--- examples/pwa-docs/.vitepress/config.ts
+++ examples/pwa-docs/.vitepress/config.ts
@@ -1,63 +1,65 @@
 import { defineConfig } from '@vite-pwa/vitepress'
 
 export default defineConfig({
-    vite: {
-        logLevel: 'info'
+  vite: {
+    logLevel: 'info',
+    define: {
+      __DATE__: `'${new Date().toISOString()}'`,
     },
-    lang: 'en-US',
-    title: 'VitePress PWA',
-    description: 'Vite Plugin PWA Integration example for VitePress',
-    head: [
-        ['meta', { name: 'theme-color', content: '#ffffff' }],
-        ['link', { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' }],
-        ['link', { rel: 'mask-icon', href: '/favicon.svg', color: '#ffffff' }],
-        ['meta', {
-            name: 'keywords',
-            content: 'PWA, VitePress, workbox, Vite, vite-plugin',
-        }],
-        ['link', { rel: 'apple-touch-icon', href: '/pwa-192x192.png', sizes: '192x192' }],
-        ['link', { rel: 'manifest', href: '/manifest.webmanifest', sizes: '192x192' }],
-        ['script', { src: '/registerSW.js' }],
+  },
+  lang: 'en-US',
+  title: 'VitePress PWA',
+  description: 'Vite Plugin PWA Integration example for VitePress',
+  head: [
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+    ['link', { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' }],
+    ['link', { rel: 'mask-icon', href: '/favicon.svg', color: '#ffffff' }],
+    ['meta', {
+      name: 'keywords',
+      content: 'PWA, VitePress, workbox, Vite, vite-plugin',
+    }],
+    ['link', { rel: 'apple-touch-icon', href: '/pwa-192x192.png', sizes: '192x192' }],
+    ['link', { rel: 'manifest', href: '/manifest.webmanifest', sizes: '192x192' }],
+    ['script', { src: '/registerSW.js' }],
+  ],
+  themeConfig: {
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2022-PRESENT Anthony Fu',
+    },
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about', activeMatch: '/about' },
     ],
-    themeConfig: {
-        footer: {
-            message: 'Released under the MIT License.',
-            copyright: 'Copyright © 2022-PRESENT Anthony Fu',
+  },
+  pwa: {
+    mode: 'development',
+    base: '/',
+    scope: '/',
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.svg'],
+    manifest: {
+      name: 'VitePress PWA',
+      short_name: 'VitePressPWA',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
         },
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'About', link: '/about', activeMatch: '/about.html' },
-        ],
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
     },
-    pwa: {
-        mode: 'development',
-        base: '/',
-        scope: '/',
-        registerType: 'autoUpdate',
-        injectRegister: 'script',
-        includeAssets: ['favicon.svg'],
-        manifest: {
-            name: 'VitePress PWA',
-            short_name: 'VitePressPWA',
-            theme_color: '#ffffff',
-            icons: [
-                {
-                    src: 'pwa-192x192.png',
-                    sizes: '192x192',
-                    type: 'image/png',
-                },
-                {
-                    src: 'pwa-512x512.png',
-                    sizes: '512x512',
-                    type: 'image/png',
-                },
-                {
-                    src: 'pwa-512x512.png',
-                    sizes: '512x512',
-                    type: 'image/png',
-                    purpose: 'any maskable',
-                },
-            ],
-        },
-    }
+  },
 })
--- examples/pwa-docs/index.md
+++ examples/pwa-docs/index.md
@@ -2,4 +2,10 @@
 title: VitePress PWA
 ---
 
+<script setup>
+const date = __DATE__
+</script>
+
 # VitePress PWA
+
+<pre>Generated at: {{ date }}</pre>
