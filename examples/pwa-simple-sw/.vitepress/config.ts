70762c327ff83df137e2b37b9537d717be74b625
Joaquín Sánchez <userquin@gmail.com>
2023-10-15 19:07:23 +0200

    chore: add experimental example using custom sw (#25)
    * chore: add experimental example using custom sw
    
    * chore: use sw location for base

---
+++ examples/pwa-simple-sw/.vitepress/config.ts
@@ -0,0 +1,85 @@
+import { defineConfig } from 'vitepress'
+import { withPwa } from '@vite-pwa/vitepress'
+
+const base = '/' // '/vite-plugin-pwa/'
+
+export default withPwa(defineConfig({
+  vite: {
+    logLevel: 'info',
+    define: {
+      __DATE__: `'${new Date().toISOString()}'`,
+    },
+  },
+  base,
+  lang: 'en-US',
+  title: 'VitePress PWA',
+  description: 'Vite Plugin PWA Integration example for VitePress',
+  head: [
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+    ['link', { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' }],
+    ['link', { rel: 'mask-icon', href: '/favicon.svg', color: '#ffffff' }],
+    ['meta', {
+      name: 'keywords',
+      content: 'PWA, VitePress, workbox, Vite, vite-plugin',
+    }],
+    ['link', { rel: 'apple-touch-icon', href: '/pwa-192x192.png', sizes: '192x192' }],
+  ],
+  themeConfig: {
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2022-PRESENT Anthony Fu',
+    },
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about', activeMatch: '/about' },
+      {
+        text: 'Packages',
+        items: [
+          { text: 'Foo', link: '/packages/foo' },
+          { text: 'Bar', link: '/packages/bar' },
+        ],
+      },
+    ],
+  },
+  pwa: {
+    mode: 'development',
+    registerType: 'autoUpdate',
+    strategies: 'injectManifest',
+    srcDir: '.vitepress/',
+    filename: 'sw.ts',
+    // injectRegister: 'inline',
+    includeAssets: ['favicon.svg'],
+    manifest: {
+      name: 'VitePress PWA',
+      short_name: 'VitePressPWA',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    },
+    injectManifest: {
+      globPatterns: ['**/*.{css,js,html,svg,png,ico,txt,woff2}'],
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      type: 'module',
+      navigateFallback: '/',
+    },
+  },
+}))
