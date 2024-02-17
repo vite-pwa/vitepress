ebe7cb10ff82b255d7e908928bf60f3021deaede
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2024-02-17 22:12:33 +0530

    chore(types): avoid casting and explicitly specify return type (#31)

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import { withUserConfig } from './integration'
 
 export * from './types'
 
-export async function withPwa<T = DefaultTheme.Config>(config: UserConfigExport<T>) {
+export async function withPwa<T = DefaultTheme.Config>(
+  config: UserConfigExport<T>,
+): Promise<UserConfigExport<T>> {
   if (typeof config === 'function') {
-    return <UserConfigExport<T>>(async (ctx) => {
+    return async (ctx) => {
       const userConfig = await config(ctx)
       return withUserConfig<T>(userConfig)
-    })
+    }
   }
 
   return withUserConfig<T>(await config)
